da8d64e5047622df5c44ede292f968e8f8df55de
Chamal <chamalb@99x.lk>
2017-08-30 11:39:20 +0530

    aws question help app

--- controller.js
+++ controller.js
@@ -1,33 +1,27 @@
 var app = angular.module('app', []);
 
-function mainController($scope, $http) {  
-	$scope.flip=function(index){
-		var id="#"+index+"_comment";
-		 $(id).toggleClass('flipped');
+function mainController($scope, $http) {
+	$scope.flip = function (index) {
+		var id = "#" + index + "_comment";
+		$(id).toggleClass('flipped');
 	}
-	$scope.showPanel=false;
-	$scope.showInput =function(){
-		$scope.showPanel =!	$scope.showPanel;
+	$scope.showPanel = false;
+	$scope.showInput = function () {
+		$scope.showPanel = !$scope.showPanel;
 	}
-	$scope.questions=[];
-	var baseUrl="https://aws-question-help.herokuapp.com/";
-	$http.get(baseUrl+"getQuestions")
-   .then(
-       function(response){ 
-		$scope.questions=response.data;
-       }, 
-       function(response){
-         $scope.questions=[
-	{
-		question : "Currently we system is under maintanance... soon you will be able to see questions...!",
-		answer_type:'radio',
-		tags:["tech","error","service"],
-		answer:["Currently we system is under maintanance... soon you will be able to see questions...!"],
-		comments:[
-		{"user":"Chamal Pradeep","content" : "Currently we system is under maintanance... soon you will be able to see questions...!","date":"2017-08-29"}
-		]
-	}]
-       }
-    );
+	$scope.random = function () {
+		return 0.5 - Math.random();
+	}
+	$scope.questions = [];
+	var baseUrl = "https://aws-question-help.herokuapp.com/";
+	$http.get(baseUrl + "getQuestions")
+		.then(
+			function (response) {
+				$scope.questions = response.data;
+			},
+			function (response) {
+				 
+			}
+		);
 
 }
